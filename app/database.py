c15d72ba58ff7b8968dbe0e2a9793df7cda4acd2
50001-Satya Krishna <tdsatyakrishna99@gmail.com>
2026-08-12 18:27:41 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -10,8 +10,10 @@ from app.config import settings
 
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql://{settings.database_username}:{settings.database_password}@"
-    f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+    f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require"
 )
+# In app/database.py
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
